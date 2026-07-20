fa6dbdd5c7bc5baa71540feba1b22d6cdbb770ce
Nicholas Belous <118130829+nicholasbelous@users.noreply.github.com>
2026-07-19 20:56:25 -0400

    Add: reverse-string - submission-0

---
+++ Data Structures & Algorithms/reverse-string/submission-0.py
@@ -0,0 +1,18 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+
+        left, right = 0, len(s) - 1
+
+        while left < right:
+            pointer = s[left]
+            s[left] = s[right]
+            s[right] = pointer
+
+            left += 1
+            right -= 1
+
+        return s
+        
