6fa87cb074ace5d538f89fa47703b7bbc96ed6df
Nicholas Belous <118130829+nicholasbelous@users.noreply.github.com>
2026-07-20 23:11:45 -0400

    Add: valid-palindrome-ii - submission-3

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-3.py
@@ -0,0 +1,25 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        left, right = 0, len(s) - 1
+
+        deleteUsed = False
+        firstCheck = False
+
+        while left < right:
+            if(s[left] == s[right]):
+                left += 1
+                right -= 1
+            else:
+                if deleteUsed == False:
+                    deleteUsed = True
+                    lHold, rHold = left, right
+                    right -= 1
+                elif(deleteUsed == False and firstCheck == False):
+                    firstCheck = True
+                    left = lHold + 1
+                    right = rHold
+                else:
+                    return False
+                
+
+        return True
