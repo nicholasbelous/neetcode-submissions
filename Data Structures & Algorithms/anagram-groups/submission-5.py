9a65fc44b78a87858a5038f93ba0d3f24f79fcca
Nicholas Belous <118130829+nicholasbelous@users.noreply.github.com>
2026-08-15 18:31:35 -0400

    Add: anagram-groups - submission-5

---
+++ Data Structures & Algorithms/anagram-groups/submission-5.py
@@ -0,0 +1,19 @@
+from collections import defaultdict, Counter 
+from types import MappingProxyType 
+
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        
+        groups = defaultdict(list)
+
+        for s in strs:
+            groups[frozenset(Counter(s))].append(s)
+        
+        final_lis = []
+
+
+        for v in groups.values():
+            final_lis.append(v)
+
+        return final_lis
