d52e33e631adce9ded218dd764ef69688627b8e3
Nicholas Belous <118130829+nicholasbelous@users.noreply.github.com>
2026-09-07 16:19:06 -0400

    Add: permutation-string - submission-6

---
+++ Data Structures & Algorithms/permutation-string/submission-6.py
@@ -0,0 +1,20 @@
+from collections import Counter
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        s1_counts = Counter(s1)
+        s2_counts = Counter(s2)
+
+        if s1_counts <= s2_counts:
+            window_right = len(s1)
+            window_left = 0
+            window = s2[window_left:window_right]
+
+            while window_right <= len(s2):
+                if(Counter(window) == s1_counts):
+                    return True
+
+                window_right += 1
+                window_left += 1
+                window = s2[window_left:window_right]
+        
+        return False
